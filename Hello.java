45715e9591c9d68c37334676130f55dd0b7fed04
Aditya <dibyaadityarout@gmail.com>
2025-03-01 17:57:46 +0530

    Update Hello.java with new logic

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
 public class Hello{
   public static void main(String args[]) {
-	System.out.println("Hello Git!");
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter n:");
+	int n=sc.nextInt();
+	int m=16;
+	System.out.println(m+n);
   }
 }
