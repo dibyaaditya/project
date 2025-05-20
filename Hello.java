8cf327302035d3f0a445e11406be66b957108ecc
Aditya <dibyaadityarout@gmail.com>
2025-05-20 13:48:47 +0530

    Updated Hello.java

--- Hello.java
+++ Hello.java
@@ -6,9 +6,10 @@ public class Hello{
 	int n=sc.nextInt();
 	for(int i=1; i<=n; i++){
 	    for(int j=1; j<=i; j++){
-		System.out.print("*");
+		System.out.print("* ");
 		}
 	System.out.println();
 	}
+	sc.close();
   }
 }
