5bf2f6149a0e9ef80eda48c1613058ffc2e1245a
Aditya <dibyaadityarout@gmail.com>
2025-05-23 20:07:52 +0530

    Correct the Program

--- Main.java
+++ Main.java
@@ -3,12 +3,12 @@ public class Main{
   public static void main(String args[]){
 	  Scanner sc = new Scanner(System.in);
 	  System.out.println("Enter the amount of water you drink per day in Litre:");
-	  double ml = sc.nextInt();
-	  if(ml>=4.0){
+	  int ml = sc.nextInt();
+	  if(ml>4){
 	     System.out.println("Too Much");
-	  } else if(ml<=4.0){
+	  } else if(ml<4){
 		System.out.println("Not Much");
-	    } else {
+	    } else if(ml==4){
 		   System.out.println("Good");
 	      }
 	sc.close();
