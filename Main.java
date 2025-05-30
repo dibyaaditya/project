c7ec7dbfd29a40cfc9a7ff4c1f5a1ac0ef34cb3d
Aditya <dibyaadityarout@gmail.com>
2025-05-30 08:04:09 +0530

    If else ladder concept updated

--- Main.java
+++ Main.java
@@ -4,13 +4,14 @@ public class Main{
 	  Scanner sc = new Scanner(System.in);
 	  System.out.println("Enter the amount of water you drink per day in Litre:");
 	  int ml = sc.nextInt();
-	  if(ml>4){
-	     System.out.println("Too Much");
-	  } else if(ml<4){
-		System.out.println("Not Much");
-	    } else if(ml==4){
-		   System.out.println("Good");
-	      }
-	sc.close();
+	  if(ml==4){
+		System.out.println("Good");
+	  }else if(ml>4){
+		System.out.println("Too Much");
+	  }else{
+		System.out.println("Drink Water");
+	  }
+		sc.close();
+	  }
+	
   }
-}
