70755f0b11f7d13853892d05c1c63f81494f3ddb
Aditya <dibyaadityarout@gmail.com>
2025-06-11 15:13:57 +0530

    Learning Array

--- Main.java
+++ Main.java
@@ -1,16 +1,16 @@
-import java.util.Scanner;
 public class Main{
   public static void main(String args[]){
-	  Scanner sc = new Scanner(System.in);
-	  System.out.println("Enter the amount of water you drink per day in Litre:");
-	  int ml = sc.nextInt();
-	  if(ml==4){
-		System.out.println("Good");
-	  }else if(ml>4){
-		System.out.println("Too Much");
-	  }else{
-		System.out.println("Drink Water");
-	  }
-		sc.close();
+	//   Scanner sc = new Scanner(System.in);
+	//   System.out.println("Enter the amount of water you drink per day in Litre:");
+	//   int ml = sc.nextInt();
+	//   if(ml==4){
+	// 	System.out.println("Good");
+	//   }else if(ml>4){
+	// 	System.out.println("Too Much");
+	//   }else{
+	// 	System.out.println("Drink Water");
+	//   }
+	// 	sc.close();
+	System.out.println(args[2]);
 	  }
   }
