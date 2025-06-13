fe7798090627d4dc0080c4a3a7e21a94fc522ca7
Aditya <dibyaadityarout@gmail.com>
2025-06-13 20:28:42 +0530

    Learning datatypes,typeconversion

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 public class Main{
   public static void main(String args[]){
-	//   Scanner sc = new Scanner(System.in);
+	   Scanner sc = new Scanner(System.in);
 	//   System.out.println("Enter the amount of water you drink per day in Litre:");
 	//   int ml = sc.nextInt();
 	//   if(ml==4){
@@ -11,6 +12,26 @@ public class Main{
 	// 	System.out.println("Drink Water");
 	//   }
 	// 	sc.close();
-	System.out.println(args[2]);
+	// System.out.println(args[2]);
+	// int num = (int)(69.75);
+	// System.out.println(num);
+	int num = sc.nextInt();
+	float fl = 56.77f;
+	double d = 6577.57;
+	char ch = 'A';
+	float f2 = sc.nextFloat();
+	double d2 = sc.nextDouble();
+	String s1 = "Aditya Rout";
+	String s2 = sc.nextLine();
+	int num2 = 54;
+	int sum = num + num2;
+	System.out.println(num+"\n"+num2+"\n"+sum);
+	System.out.println(d+d2);
+	System.out.println(s1+"\n"+s2);
+	System.out.println(fl+f2);
+	System.out.println(ch);
+	sc.close();
+
+
 	  }
   }
